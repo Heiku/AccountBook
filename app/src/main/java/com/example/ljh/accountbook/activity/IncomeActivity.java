9cb34e48b4fba2f616362e9f79c9d58b047dc86f
Heiku <1154034741@qq.com>
2017-05-27 11:43:47 +0800

    补充

--- app/src/main/java/com/example/ljh/accountbook/activity/IncomeActivity.java
+++ app/src/main/java/com/example/ljh/accountbook/activity/IncomeActivity.java
@@ -5,8 +5,16 @@ package com.example.ljh.accountbook.activity;
  */
 
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * 添加收入页面
  */
-public class IncomeActivity {
+public class IncomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/ljh/accountbook/activity/PayActivity.java
+++ app/src/main/java/com/example/ljh/accountbook/activity/PayActivity.java
@@ -4,8 +4,15 @@ package com.example.ljh.accountbook.activity;
  * Created by LJH on 2017/5/27.
  */
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * 添加支出页面
  */
-public class PayActivity {
+public class PayActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
